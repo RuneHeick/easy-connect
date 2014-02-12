50515f1fb49699370f4ef9e79aebc5fda2d03492
Saelnayin@gmail.com <Saelnayin@gmail.com@d05209d9-4594-dfb0-c321-ea6a2a2fac26>
2014-02-12 23:18:45 +0000

    Easy-Connect_test has now been added to the Repository

---
+++ gen/iha/bachelor/smo/aba/rah/easyconnect/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package iha.bachelor.smo.aba.rah.easyconnect;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
