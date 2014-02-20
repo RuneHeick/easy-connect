df42717164cdfbeaeb05ea5833481f36c3ce6fd5
soren.m.olesen@gmail.com <soren.m.olesen@gmail.com@d05209d9-4594-dfb0-c321-ea6a2a2fac26>
2014-02-20 15:48:13 +0000

    Logging class added

---
+++ ecru_kernel/ecru_kernel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using logging;
+
+namespace ecru_kernel
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+            
+            Debug.Print("are we live?");
+
+            foreach (var folders in Directory.GetDirectories(""))
+            {
+                Debug.Print(folders);
+            }
+           
+
+            // Directly start logging, no need to create any instance of Logger class
+            Logging.LogToFile = true;    // if false it will only do Debug.Print()
+            Logging.Append = true;       // will append the information to existing if any
+            Logging.PrefixDateTime = true; // add a time stamp on each Log call. Note: Netduino time is not same as clock time.
+
+            Logging.Log("lort is good!");
+
+        }
+    }
+}
