fcab828fbb77ae7c5b7460731ef2285e223b6f42
soren.m.olesen@gmail.com <soren.m.olesen@gmail.com@d05209d9-4594-dfb0-c321-ea6a2a2fac26>
2014-03-02 15:34:30 +0000

    Subscribe + Unsubscribe added with unit tests

---
+++ ecru_kernel/MessageBus.Test/MockSubscriber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBus.Test
+{
+    class MockSubscriber : ISubscriber
+    {
+        public MockSubscriber(TMessage message, TMessageHandler functionPointer)
+        {
+            FunctionPointer = functionPointer;
+            Message = message;
+        }
+
+        public MockSubscriber()
+        {
+            
+        }
+
+        public TMessage Message { get; private set; }
+        public TMessageHandler FunctionPointer { get; private set; }
+    }
+}
--- ecru_kernel/MessageBus/MessageBus.cs
+++ ecru_kernel/MessageBus/MessageBus.cs
@@ -1,32 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
-using Subscriber;
 
 namespace MessageBus
 {
-    public class MessageBus : IMessageBus
+    /// <summary>
+    /// The MessageBus class implements the Publisher/Subscriber design pattern. The class is threadsafe and does not spawn new threads. 
+    /// </summary>
+    public static class MessageBus
     {
-        
 
-        public bool Subscribe(ISubscriber subscriber)
+        private static readonly Hashtable Subscriptions = new Hashtable();
+        private static readonly Object Lock = new object();
+
+        /// <summary>
+        /// Subscribe will add the subscriber to the Subscriptions hashtable.
+        /// </summary>
+        /// <param name="subscriber"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>This method returns a bool.</returns>
+        public static bool Subscribe(ISubscriber subscriber)
         {
-            throw new NotImplementedException();
+
+            if (subscriber.Message == null) throw new ArgumentNullException("subscriber.Message");
+            var message = subscriber.Message;
+
+            if (subscriber.FunctionPointer == null) throw new ArgumentNullException("subscriber.FunctionPointer");
+            var functionPointer = subscriber.FunctionPointer;
+
+            if (Subscriptions.ContainsKey(message))
+            {
+                Debug.Print("MessageBus - Message in subscriptions");
+                lock (Lock)
+                {
+                    var list = Subscriptions[message] as ArrayList;
+                    if (list != null)
+                    {
+                        list.Add(functionPointer);
+                    }
+                }
+                return true;
+            }
+
+            Debug.Print("MessageBus - Message not subscriptions");
+            lock (Lock)
+            {
+                var list = new ArrayList { functionPointer };
+                Subscriptions.Add(message, list);
+            }
+            return true;
         }
 
-        public bool Unsubscribe(ISubscriber subscriber)
+        /// <summary>
+        /// Unsubscribe will remove the subscriber from the Subscriptions hashtable.
+        /// </summary>
+        /// <param name="subscriber"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        /// <returns>This method returns a bool.</returns>
+        public static bool Unsubscribe(ISubscriber subscriber)
         {
-            throw new NotImplementedException();
+
+            if (subscriber.Message == null) throw new ArgumentNullException("subscriber.Message");
+            var message = subscriber.Message;
+
+            if (subscriber.FunctionPointer == null) throw new ArgumentNullException("subscriber.FunctionPointer");
+            var functionPointer = subscriber.FunctionPointer;
+
+            if (!Subscriptions.ContainsKey(message))
+            {
+                throw new ArgumentException("message not in subscriptions");
+            }
+
+            Debug.Print("MessageBus - Message in subscriptions");
+            lock (Lock)
+            {
+                var list = Subscriptions[message] as ArrayList;
+                if (list == null)
+                {
+                    throw new ArgumentException("FunctionPointer list is empty");
+                }
+                list.Remove(functionPointer);
+
+                if (list.Count.Equals(0))
+                {
+                    Subscriptions.Remove(message);
+                }
+                return true;
+            }
         }
 
-        public bool Publish(IMessage message)
+        public static bool Publish(TMessage message)
         {
-            throw new NotImplementedException();
+
+            var subscribers = Subscriptions[message] as ArrayList;
+
+            foreach (var subscriber in subscribers)
+            {
+                
+            }
+
         }
 
-        
+
     }
 }
