067be76db2965804a58e6dd272bcd0b5e8bbbe94
ArbjergHeick@gmail.com <ArbjergHeick@gmail.com@d05209d9-4594-dfb0-c321-ea6a2a2fac26>
2014-03-13 14:07:06 +0000

    Remove v.4.0

--- trunk/CC2540/ECM/Projects/ble/SimpleBLEPeripheral/Source/GAPManger.h
+++
@@ -1,7 +0,0 @@
-
-
-void GAPManeger_Setup();
-
-void GAPManger_Start();
-
-uint8 GAPManget_SetupName();
--- trunk/CC2540/ECM/Projects/ble/SimpleBLEPeripheral/Source/InitState.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "State.h"
-
-extern uint16 InitState_ProcessEvent( uint8 task_id, uint16 events );
-
-extern void InitState_Enter(uint8); 
-
-extern void InitState_Exit(); 
