79de71703c39bd2a7a166216821ec72a9736d35b
ArbjergHeick@gmail.com <ArbjergHeick@gmail.com@d05209d9-4594-dfb0-c321-ea6a2a2fac26>
2014-03-13 14:44:51 +0000

    New BLE WOOOOOOO

---
+++ trunk/CC2540/ECM/Projects/ble/Profiles/DevInfo/devinfoservice.c
@@ -0,0 +1,421 @@
+/**************************************************************************************************
+  Filename:       devinfoservice.c
+  Revised:        $Date $
+  Revision:       $Revision $
+
+  Description:    This file contains the Device Information service.
+
+
+  Copyright 2012 - 2013 Texas Instruments Incorporated. All rights reserved.
+
+  IMPORTANT: Your use of this Software is limited to those specific rights
+  granted under the terms of a software license agreement between the user
+  who downloaded the software, his/her employer (which must be your employer)
+  and Texas Instruments Incorporated (the "License").  You may not use this
+  Software unless you agree to abide by the terms of the License. The License
+  limits your use, and you acknowledge, that the Software may not be modified,
+  copied or distributed unless embedded on a Texas Instruments microcontroller
+  or used solely and exclusively in conjunction with a Texas Instruments radio
+  frequency transceiver, which is integrated into your product.  Other than for
+  the foregoing purpose, you may not use, reproduce, copy, prepare derivative
+  works of, modify, distribute, perform, display or sell this Software and/or
+  its documentation for any purpose.
+
+  YOU FURTHER ACKNOWLEDGE AND AGREE THAT THE SOFTWARE AND DOCUMENTATION ARE
+  PROVIDED “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+  INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY, TITLE,
+  NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL
+  TEXAS INSTRUMENTS OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER CONTRACT,
+  NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR OTHER
+  LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+  INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE
+  OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT
+  OF SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+  (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+
+  Should you have any questions regarding your right to use this Software,
+  contact Texas Instruments Incorporated at www.TI.com.
+**************************************************************************************************/
+
+/*********************************************************************
+ * INCLUDES
+ */
+#include "bcomdef.h"
+#include "OSAL.h"
+#include "linkdb.h"
+#include "att.h"
+#include "gatt.h"
+#include "gatt_uuid.h"
+#include "gatt_profile_uuid.h"
+#include "gattservapp.h"
+
+#include "GenericValueManger.h"
+#include "devinfoservice.h"
+
+/*********************************************************************
+ * MACROS
+ */
+
+/*********************************************************************
+ * CONSTANTS
+ */
+
+/*********************************************************************
+ * TYPEDEFS
+ */
+
+/*********************************************************************
+ * GLOBAL VARIABLES
+ */
+// Device information service
+CONST uint8 devInfoServUUID[ATT_BT_UUID_SIZE] =
+{
+  LO_UINT16(DEVINFO_SERV_UUID), HI_UINT16(DEVINFO_SERV_UUID)
+};
+
+// Model Number String
+CONST uint8 devInfoModelNumberUUID[ATT_BT_UUID_SIZE] =
+{
+  LO_UINT16(MODEL_NUMBER_UUID), HI_UINT16(MODEL_NUMBER_UUID)
+};
+
+// Serial Number String
+CONST uint8 devInfoSerialNumberUUID[ATT_BT_UUID_SIZE] =
+{
+  LO_UINT16(SERIAL_NUMBER_UUID), HI_UINT16(SERIAL_NUMBER_UUID)
+};
+
+// Manufacturer Name String
+CONST uint8 devInfoMfrNameUUID[ATT_BT_UUID_SIZE] =
+{
+  LO_UINT16(MANUFACTURER_NAME_UUID), HI_UINT16(MANUFACTURER_NAME_UUID)
+};
+
+
+/*********************************************************************
+ * EXTERNAL VARIABLES
+ */
+
+/*********************************************************************
+ * EXTERNAL FUNCTIONS
+ */
+
+/*********************************************************************
+ * LOCAL VARIABLES
+ */
+
+/*********************************************************************
+ * Profile Attributes - variables
+ */
+
+// Device Information Service attribute
+static CONST gattAttrType_t devInfoService = { ATT_BT_UUID_SIZE, devInfoServUUID };
+
+// Model Number String characteristic
+static uint8 devInfoModelNumberProps = GATT_PROP_READ;
+static GenericValue devInfoModelNumber; // = "Model Number";
+
+// Serial Number String characteristic
+static uint8 devInfoSerialNumberProps = GATT_PROP_READ;
+static GenericValue devInfoSerialNumber; // = "Serial Number";
+
+// Manufacturer Name String characteristic
+static uint8 devInfoMfrNameProps = GATT_PROP_READ;
+static GenericValue devInfoMfrName; // = "Manufacturer Name som er meget meget langt fordi det er fra kina";
+
+
+/*********************************************************************
+ * Profile Attributes - Table
+ */
+
+static gattAttribute_t devInfoAttrTbl[] =
+{
+  // Device Information Service
+  {
+    { ATT_BT_UUID_SIZE, primaryServiceUUID }, /* type */
+    GATT_PERMIT_READ,                         /* permissions */
+    0,                                        /* handle */
+    (uint8 *)&devInfoService                  /* pValue */
+  },
+  
+    // Model Number String Declaration
+    {
+      { ATT_BT_UUID_SIZE, characterUUID },
+      GATT_PERMIT_READ,
+      0,
+      &devInfoModelNumberProps
+    },
+
+      // Model Number Value
+      {
+        { ATT_BT_UUID_SIZE, devInfoModelNumberUUID },
+        GATT_PERMIT_READ,
+        0,
+        NULL
+      },
+
+    // Serial Number String Declaration
+    {
+      { ATT_BT_UUID_SIZE, characterUUID },
+      GATT_PERMIT_READ,
+      0,
+      &devInfoSerialNumberProps
+    },
+
+      // Serial Number Value
+      {
+        { ATT_BT_UUID_SIZE, devInfoSerialNumberUUID },
+        GATT_PERMIT_READ,
+        0,
+        NULL
+      },
+
+    // Manufacturer Name String Declaration
+    {
+      { ATT_BT_UUID_SIZE, characterUUID },
+      GATT_PERMIT_READ,
+      0,
+      &devInfoMfrNameProps
+    },
+
+      // Manufacturer Name Value
+      {
+        { ATT_BT_UUID_SIZE, devInfoMfrNameUUID },
+        GATT_PERMIT_READ,
+        0,
+        NULL
+      }
+};
+
+
+/*********************************************************************
+ * LOCAL FUNCTIONS
+ */
+static uint8 devInfo_ReadAttrCB( uint16 connHandle, gattAttribute_t *pAttr,
+                            uint8 *pValue, uint8 *pLen, uint16 offset, uint8 maxLen );
+
+/*********************************************************************
+ * PROFILE CALLBACKS
+ */
+// Device Info Service Callbacks
+CONST gattServiceCBs_t devInfoCBs =
+{
+  devInfo_ReadAttrCB, // Read callback function pointer
+  NULL,               // Write callback function pointer
+  NULL                // Authorization callback function pointer
+};
+
+/*********************************************************************
+ * NETWORK LAYER CALLBACKS
+ */
+
+/*********************************************************************
+ * PUBLIC FUNCTIONS
+ */
+
+/*********************************************************************
+ * @fn      DevInfo_AddService
+ *
+ * @brief   Initializes the Device Information service by registering
+ *          GATT attributes with the GATT server.
+ *
+ * @return  Success or Failure
+ */
+bStatus_t DevInfo_AddService()
+{
+  
+  // Register GATT attribute list and CBs with GATT Server App
+  return GATTServApp_RegisterService( devInfoAttrTbl,
+                                      GATT_NUM_ATTRS( devInfoAttrTbl ),
+                                      &devInfoCBs );
+}
+
+/*********************************************************************
+ * @fn      DevInfo_SetParameter
+ *
+ * @brief   Set a Device Information parameter.
+ *
+ * @param   param - Profile parameter ID
+ * @param   len - length of data to write
+ * @param   value - pointer to data to write.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ *
+ * @return  bStatus_t
+ */
+bStatus_t DevInfo_SetParameter( uint8 param, uint8 len, void *value )
+{
+  bStatus_t ret = SUCCESS;
+
+  switch ( param )
+  {
+    
+  case DEVINFO_MODEL_NUMBER:
+    if(devInfoModelNumber.status == NOT_INIT && GenericValue_SetValue(&devInfoModelNumber, value, len));
+    else 
+    {
+      if(devInfoModelNumber.size == len && 0==memcmp(devInfoModelNumber.pValue, value,len)) 
+        return SUCCESS;
+      return FAILURE;
+    }
+    break;
+  case DEVINFO_SERIAL_NUMBER:
+    if(devInfoSerialNumber.status == NOT_INIT && GenericValue_SetValue(&devInfoSerialNumber, value, len));
+    else 
+    {
+      if(devInfoSerialNumber.size == len && 0==memcmp(devInfoSerialNumber.pValue, value,len)) 
+        return SUCCESS;
+      return FAILURE;
+    }
+    break;
+  case DEVINFO_MANUFACTURER_NAME:
+    if(devInfoMfrName.status == NOT_INIT && GenericValue_SetValue(&devInfoMfrName, value, len));
+    else 
+    {
+      if(devInfoMfrName.size == len && 0==memcmp(devInfoMfrName.pValue, value,len)) 
+        return SUCCESS;
+      return FAILURE;
+    }
+    break;
+    
+    default:
+      ret = INVALIDPARAMETER;
+      break;
+  }
+
+  return ( ret );
+}
+
+/*********************************************************************
+ * @fn      DevInfo_GetParameter
+ *
+ * @brief   Get a Device Information parameter.
+ *
+ * @param   param - Profile parameter ID
+ * @param   value - pointer to data to get.  This is dependent on
+ *          the parameter ID and WILL be cast to the appropriate
+ *          data type (example: data type of uint16 will be cast to
+ *          uint16 pointer).
+ *
+ * @return  bStatus_t
+ */
+bStatus_t DevInfo_GetParameter( uint8 param, void *value )
+{
+  bStatus_t ret = SUCCESS;
+
+  switch ( param )
+  {
+    case DEVINFO_MODEL_NUMBER:
+      osal_memcpy(value, devInfoModelNumber.pValue, devInfoModelNumber.size);
+      break;
+    case DEVINFO_SERIAL_NUMBER:
+      osal_memcpy(value, devInfoSerialNumber.pValue, devInfoSerialNumber.size);
+      break;
+
+    case DEVINFO_MANUFACTURER_NAME:
+      osal_memcpy(value, devInfoMfrName.pValue, devInfoMfrName.size);
+      break;
+
+    default:
+      ret = INVALIDPARAMETER;
+      break;
+  }
+
+  return ( ret );
+}
+
+/*********************************************************************
+ * @fn          devInfo_ReadAttrCB
+ *
+ * @brief       Read an attribute.
+ *
+ * @param       connHandle - connection message was received on
+ * @param       pAttr - pointer to attribute
+ * @param       pValue - pointer to data to be read
+ * @param       pLen - length of data to be read
+ * @param       offset - offset of the first octet to be read
+ * @param       maxLen - maximum length of data to be read
+ *
+ * @return      Success or Failure
+ */
+static uint8 devInfo_ReadAttrCB( uint16 connHandle, gattAttribute_t *pAttr,
+                            uint8 *pValue, uint8 *pLen, uint16 offset, uint8 maxLen )
+{
+  bStatus_t status = SUCCESS;
+  uint16 uuid = BUILD_UINT16( pAttr->type.uuid[0], pAttr->type.uuid[1]);
+
+  switch (uuid)
+  {
+    case MODEL_NUMBER_UUID:
+      if(devInfoModelNumber.status != READY)
+      {
+         status = ATT_ERR_UNLIKELY; 
+      }
+      // verify offset
+      else if (offset >= (devInfoModelNumber.size-1))
+      {
+        status = ATT_ERR_INVALID_OFFSET;
+      }
+      else
+      {
+        // determine read length (exclude null terminating character)
+        *pLen = MIN(maxLen, (devInfoModelNumber.size - 1) - offset);
+
+        // copy data
+        osal_memcpy(pValue, &devInfoModelNumber.pValue[offset], *pLen);
+      }
+      break;
+
+    case SERIAL_NUMBER_UUID:
+      if(devInfoSerialNumber.status != READY)
+      {
+         status = ATT_ERR_UNLIKELY; 
+      }
+      // verify offset
+      else if (offset >= (devInfoSerialNumber.size - 1))
+      {
+        status = ATT_ERR_INVALID_OFFSET;
+      }
+      else
+      {
+        // determine read length (exclude null terminating character)
+        *pLen = MIN(maxLen, (devInfoSerialNumber.size - 1) - offset);
+
+        // copy data
+        osal_memcpy(pValue, &devInfoSerialNumber.pValue[offset], *pLen);
+      }
+      break;
+
+
+    case MANUFACTURER_NAME_UUID:
+      if(devInfoMfrName.status != READY)
+      {
+         status = ATT_ERR_UNLIKELY; 
+      }
+      else if (offset >= (devInfoMfrName.size - 1))
+      {
+        status = ATT_ERR_INVALID_OFFSET;
+      }
+      else
+      {
+        // determine read length (exclude null terminating character)
+        *pLen = MIN(maxLen, (devInfoMfrName.size - 1) - offset);
+        
+        // copy data
+        osal_memcpy(pValue, &devInfoMfrName.pValue[offset], *pLen);
+      }
+      break;
+
+    default:
+      *pLen = 0;
+      status = ATT_ERR_ATTR_NOT_FOUND;
+      break;
+  }
+
+  return ( status );
+}
+
+
+/*********************************************************************
+*********************************************************************/
---
+++ trunk/CC2540/ECM/Projects/ble/SimpleBLEPeripheral/Source/InitState.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "State.h"
+
+extern uint16 InitState_ProcessEvent( uint8 task_id, uint16 events );
+
+extern void InitState_Enter(uint8); 
+
+extern void InitState_Exit(); 
