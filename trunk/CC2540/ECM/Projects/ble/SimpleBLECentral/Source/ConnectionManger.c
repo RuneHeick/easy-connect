773d12066a1e2faedead3a55ba68ccb0e38bad5d
ArbjergHeick@gmail.com <ArbjergHeick@gmail.com@d05209d9-4594-dfb0-c321-ea6a2a2fac26>
2014-03-17 22:13:22 +0000

    ConnectionManger.c Updated

--- trunk/CC2540/ECM/Projects/ble/SimpleBLECentral/Source/ConnectionManger.c
+++ trunk/CC2540/ECM/Projects/ble/SimpleBLECentral/Source/ConnectionManger.c
@@ -18,20 +18,55 @@
 
 #define MAX_HW_SUPPORTED_DEVICES 3
 
+typedef enum
+{
+  NOTCONNECTED,
+  CONNECTING,
+  CONNECTED, 
+  INUSE
+}ConnectionStatus_t;
+
+typedef enum
+{
+  Read,
+  Write,
+  Connect,
+  Disconnect,
+  
+}EventType_t;
+
 typedef struct 
 {
   uint8 addr[B_ADDR_LEN];
   uint16 ConnHandel;
-  bool InUse; 
+  ConnectionStatus_t status; 
 }ConnectedDevice_t; 
+ 
+
+typedef struct EventQueueItemBase_t 
+{
+  uint8 addr[B_ADDR_LEN];
+  EventType_t action;
+  
+  struct EventQueueItemBase_t* next;
+  
+}EventQueueItemBase_t;
 
 
+typedef struct EventQueueRWItem_t 
+{
+  EventQueueItemBase_t base;
+  
+  uint16 handel;
+  uint8* data;
+  uint8 length; 
+}EventQueueRWItem_t;
 
 static ConnectedDevice_t connectedDevices[MAX_HW_SUPPORTED_DEVICES];
 static uint8 ConnectedIndex = 0; 
 static uint8 ConnectedCount = 0; 
 
-
+static EventQueueItemBase_t* EventQueue = NULL; 
 
 static void EstablishLink(ConnectedDevice_t* conContainor);
 static void AcceptLink(uint8* addr, uint16 connHandel);
@@ -41,21 +76,19 @@ void CreateConnection(uint8* addr)
     uint8 i; 
     for(i = 0; i<MAX_HW_SUPPORTED_DEVICES; i++)
     {
-      if(connectedDevices[i].InUse == false)
+      if(connectedDevices[i].status != INUSE)
       {
         osal_memcpy(connectedDevices[i].addr,addr,B_ADDR_LEN); 
         EstablishLink(&connectedDevices[i]); 
         return; 
       }
     }
-    
-
-     // fix me Some Error or correction.. maybe a queue  
+    // fix me Some Error or correction.. maybe a queue  
 }
 
 static void EstablishLink(ConnectedDevice_t* conContainor)
 {
-  conContainor->InUse = true;
+  conContainor->status = CONNECTING;
   
   if(conContainor->ConnHandel != GAP_CONNHANDLE_INIT)
   {
@@ -77,8 +110,69 @@ static void AcceptLink(uint8* addr, uint16 connHandel)
       if(osal_memcmp(connectedDevices[i].addr,addr,B_ADDR_LEN))
       {
         connectedDevices[i].ConnHandel = connHandel;
+        connectedDevices[i].status = CONNECTED;
         return; 
       }
    }
+}
+
+static void addToQueue(EventQueueItemBase_t* item)
+{
+  if(EventQueue==NULL)
+  {
+    EventQueue = item; 
+  }
+  else
+  {
+    EventQueueItemBase_t* tempItem = EventQueue; 
+    
+    while(tempItem->next!=NULL)
+    {
+      tempItem = tempItem->next;
+    }
+    
+    tempItem->next = item;
+  }
+}
+
+//***********************************************************
+//      Public Queue Functions
+//***********************************************************
+
+
+void Queue_addWrite(uint8* write, uint8 len, uint8* addr, uint16 handel)
+{
+  EventQueueRWItem_t* item = (EventQueueRWItem_t*)osal_mem_alloc(sizeof(EventQueueRWItem_t)); 
+  if(item)
+  {
+    item->base.action = Write;
+    item->base.next = NULL;
+    
+    osal_memcpy(item->base.addr,addr,B_ADDR_LEN);
+    
+    item->handel = handel;
+    item->data = osal_mem_alloc(len);
+    if(item->data)
+    {
+      osal_memcpy(item->data,write,len);
+      item->length = len;
+      addToQueue((EventQueueItemBase_t*)item);
+      return;
+    }
+    else
+    {
+      osal_mem_free(item);
+    }
+  }
+}
+void Queue_addRead(uint8* addr, uint16 handel )
+{
+  
+  
+}
+
+void Queue_addServiceDiscovery(uint8* addr )
+{
+  
   
 }
